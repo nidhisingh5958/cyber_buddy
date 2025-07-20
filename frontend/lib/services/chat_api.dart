e533d3dc5e24563dea523c83f232d256fef1a129
nidhisingh5958 <nidhisingh5958@gmail.com>
2025-07-20 22:57:19 +0530

    chnages in ui

--- frontend/lib/services/chat_api.dart
+++ frontend/lib/services/chat_api.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ChatService {
-  static const String baseUrl = 'http://192.168.1.7:8000';
+  static const String baseUrl = 'http://127.0.0.1:8000';
 
   static Future<String> sendMessage(String message) async {
     final url = Uri.parse('$baseUrl/chat');
